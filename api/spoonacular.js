81881a56362041a13d52b65e7fe7ab278fca8f56
Ernst Samuel <samuel.ernst@wibilea.ch>
2026-06-10 14:23:33 +0200

    Add Vercel proxy for Spoonacular CORS fix

---
+++ api/spoonacular.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  const path = req.query.path || '';
+  const params = { ...req.query };
+  delete params.path;
+ 
+  params.apiKey = process.env.VITE_SPOONACULAR_KEY;
+ 
+  const query = new URLSearchParams(params).toString();
+  const url = `https://api.spoonacular.com/${path}?${query}`;
+ 
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(response.status).json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Proxy error', detail: err.message });
+  }
+}
